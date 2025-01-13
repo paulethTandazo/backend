258da9d60203a85944e16ef5fb15d62fb38312dc
paulethTandazo <117050687+paulethTandazo@users.noreply.github.com>
2025-01-13 18:08:37 -0500

    cambios

--- main/views.py
+++ main/views.py
@@ -7,3 +7,44 @@ def index(request):
     #return HttpResponse("Hello, World!")
     #return render(request,'main/base.html')
     return render(request, 'main/index.html')
+
+def index(request):
+        
+     # Objeto con los datos a renderizar
+     data = {
+         'title': 'Landing - Dashboard',
+     }
+     # Renderización en la plantilla
+     return render(request, 'main/index.html', data)
+
+# Importe requests y json
+import requests
+import json
+def index(request):
+        
+     # Arme el endpoint del REST API
+     current_url = request.build_absolute_uri()
+     url = current_url + '/api/v1/meowjapan'
+
+     # Petición al REST API
+     response_http = requests.get(url)
+     response_dict = json.loads(response_http.content)
+
+     print("Endpoint ", url)
+     print("Response ", response_dict)
+
+     # Respuestas totales
+     total_responses = len(response_dict.keys())
+
+     # Valores de la respuesta
+     responses = response_dict.values()
+
+     # Objeto con los datos a renderizar
+     data = {
+         'title': 'Landing - Dashboard',
+         'total_responses': total_responses,
+         'responses': responses
+     }
+
+     # Renderización en la plantilla
+     return render(request, 'main/index.html', data)
